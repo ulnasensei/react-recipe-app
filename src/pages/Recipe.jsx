f258897a49e4a7c8eba80a39a8b80aff9863b3af
Ulna <g.alperenkurt@gmail.com>
2022-10-25 23:31:33 +0300

    import fix

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useLocation, useCallback } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from "react";
+import { useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
